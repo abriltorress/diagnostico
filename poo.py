a2c9d4fc75e60d438e8157fe804309bd88c5ea86
˗ˏˋ ★ ˎˊ˗ <161755490+abriltorress@users.noreply.github.com>
2024-03-04 06:55:02 -0800

    Create poo.py

---
+++ poo.py
@@ -0,0 +1,19 @@
+class Vehiculo:
+  def __init__(self, patente, marca, modelo, kilometraje)
+  self.patente = patente
+  self.marca = marca
+  self.modelo = modelo
+  self.kilometraje = kilometraje
+
+def get_patente(self):
+  return self.patente
+
+def get_kilometraje (self, nuevo_kilometraje)
+self.kilometraje = nuevo_kilometraje
+print ("el kilometraje ha sido actualizado.")
+
+auto1 = Vehiculo ("123ABC", "Toyota", "Corolla",5000)
+print ("Patente del vehiculo", auto1.get_patente())
+print ("kilometraje actual", auto1.kilometraje)
+auto1.set_kilometraje(5500)
+print ("nuevo kilometraje", auto1.kilometraje)
